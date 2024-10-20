a17622489f662ce73d20fd9dc918bb50e60f7216
DavidRomeroN <davidnina658@gmail.com>
2024-10-20 17:19:41 -0500

    consistencia de report

--- ms-BillingReports/src/main/java/com/example/msbillingreports/Controller/ReportController.java
+++ ms-BillingReports/src/main/java/com/example/msbillingreports/Controller/ReportController.java
@@ -1,10 +1,18 @@
 package com.example.msbillingreports.Controller;
 
+import com.example.msbillingreports.Dto.ErrorResponseDto;
+import com.example.msbillingreports.Dto.SubscriptionDto;
+import com.example.msbillingreports.Dto.UserDto;
+import com.example.msbillingreports.Entity.Billing;
 import com.example.msbillingreports.Entity.Report;
+import com.example.msbillingreports.Feign.UserSubscriptionFeign;
 import com.example.msbillingreports.Service.ReportService;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -26,7 +34,24 @@ public class ReportController {
     }
     @PostMapping
     public ResponseEntity<Report> create(@RequestBody Report report) {
-        return ResponseEntity.ok(reportService.save(report));
+        try {
+            UserDto userDto = UserSubscriptionFeign.listUserById(report.getUserId()).getBody();
+
+            if (userDto == null || userDto.getId() == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ErrorResponseDto("Error: usuario no encontrada."));
+            }
+
+            Report newReport = reportService.save(report);
+            return ResponseEntity.ok(newReport);
+
+        } catch (FeignException.NotFound e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponseDto("Error: No se encontró el usuario con el ID proporcionado."));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponseDto("Error al procesar la solicitud, revise la existencia de la suscripción."));
+        }
     }
     @PutMapping("/{id}")
     public ResponseEntity<Report> update(@PathVariable Integer id,
