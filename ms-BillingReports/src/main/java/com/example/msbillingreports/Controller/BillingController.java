21a4b2cc022b8291c3593c48b21d0d04fc7c1378
DavidRomeroN <davidnina658@gmail.com>
2024-10-19 23:56:43 -0500

    nuevo cambio en billing

--- ms-BillingReports/src/main/java/com/example/msbillingreports/Controller/BillingController.java
+++ ms-BillingReports/src/main/java/com/example/msbillingreports/Controller/BillingController.java
@@ -1,8 +1,15 @@
 package com.example.msbillingreports.Controller;
 
+import com.example.msbillingreports.Dto.ErrorResponseDto;
+import com.example.msbillingreports.Dto.SubscriptionDto;
 import com.example.msbillingreports.Entity.Billing;
+import com.example.msbillingreports.Feign.UserSubscriptionFeign;
+import com.example.msbillingreports.Repository.BillingRepository;
 import com.example.msbillingreports.Service.BillingService;
+import com.example.msbillingreports.Service.ReportService;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +22,12 @@ public class BillingController {
     @Autowired
     private BillingService billingService;
 
+    @Autowired
+    private UserSubscriptionFeign userSubscriptionFeign;
+
+    @Autowired
+    private ReportService reportService;
+
     @GetMapping
     public ResponseEntity<List<Billing>> getAll() {
         return ResponseEntity.ok(billingService.list());
@@ -23,10 +36,37 @@ public class BillingController {
     public ResponseEntity<Optional<Billing>> getById(@PathVariable Integer id) {
         return ResponseEntity.ok(billingService.findById(id));
     }
+
     @PostMapping
     public ResponseEntity<Billing> create(@RequestBody Billing billing) {
-        return ResponseEntity.ok(billingService.save(billing));
+        try {
+            SubscriptionDto subscriptionDto = userSubscriptionFeign.getById(billing.getSubscriptionId()).getBody();
+
+            // Si el usuario no existe, se puede considerar que userDto es nulo
+            if (subscriptionDto == null || subscriptionDto.getId() == null) {
+                String errorMessage = "Error: Usuario no encontrado.";
+                ErrorResponseDto errorResponse = new ErrorResponseDto(errorMessage);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+
+            // Si el usuario es válido, guardar la notificación
+            Billing newNotification = billingService.save(billing);
+            return ResponseEntity.ok(newNotification);
+
+        } catch (FeignException.NotFound e) {
+            // Manejo específico del error 404
+            String errorMessage = "Error: Suscripcion no encontrado.";
+            ErrorResponseDto errorResponse = new ErrorResponseDto(errorMessage);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            // Manejo genérico de excepciones
+            String errorMessage = "Error al procesar la solicitud, revise la existencia de la suscripcion";
+            ErrorResponseDto errorResponse = new ErrorResponseDto(errorMessage);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<Billing> update(@PathVariable Integer id,
                                          @RequestBody Billing billing) {
--- ms-BillingReports/src/main/java/com/example/msbillingreports/Feign/UserSubscriptionFeign.java
+++ ms-BillingReports/src/main/java/com/example/msbillingreports/Feign/UserSubscriptionFeign.java
@@ -12,6 +12,9 @@ public interface UserSubscriptionFeign {
     @GetMapping("/subscription/{id}")
     public ResponseEntity<SubscriptionDto> listSubById(@PathVariable Integer id);
 
+    @GetMapping("/{id}") // Falta esta anotación para corregir el error
+    ResponseEntity<UserDto> getById(@PathVariable Integer id);
+
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDto> listUserById(@PathVariable Integer id);
 }
